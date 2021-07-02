c3d0fb43c442c98335c8af03a508541a993aa615
harper-joo-lee <lamantciel@naver.com>
2021-07-03 01:41:52 +0900

    initial video

---
+++ vue2/src/components/VideoList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="video_list">
+      <ul>
+        <li v-for="(item, idx) in videoList" v-bind:key="idx" >
+          {{ item.url }}
+        </li>
+      </ul>
+    </div>
+</template>
+
+
+
+
+
+
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { VideoType } from '../type/VideoInterface'
+
+
+@Component 
+export default class VideoList extends Vue {
+  @Prop() videoList!: VideoType[];
+
+}
+
+</script>
---
+++ vue2/src/type/VideoInterface.ts
@@ -0,0 +1,5 @@
+export interface VideoType {
+    name: string;
+    url: string;
+}
+
